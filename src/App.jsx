2a4f3e1c2280902344f5b608028046509d7b7f5a
Hojiakbar11 <hojiakbaraziov092@gmail.com>
2024-08-05 15:04:43 +0300

    mtf

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='text-center text-4xl font-bold text-white'>Kursda o'tiladigan texnologiyalar</h1>
+    </div>
   )
 }
 
